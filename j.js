458e26f2bf486fc8835d803539d20120a601e13f
x3masks <turkeevm@gmail.com>
2016-10-27 01:05:08 +0300

    add menu; reworked header; add about;

--- j.js
+++ j.js
@@ -1,1 +1,1 @@
-!function(n){function r(o){if(t[o])return t[o].exports;var e=t[o]={exports:{},id:o,loaded:!1};return n[o].call(e.exports,e,e.exports,r),e.loaded=!0,e.exports}var t={};return r.m=n,r.c=t,r.p="/",r(0)}({0:function(n,r,t){t(1),t(10)},1:function(n,r){},10:function(n,r){}});
+!function(n){function r(o){if(t[o])return t[o].exports;var e=t[o]={exports:{},id:o,loaded:!1};return n[o].call(e.exports,e,e.exports,r),e.loaded=!0,e.exports}var t={};return r.m=n,r.c=t,r.p="/",r(0)}({0:function(n,r,t){t(1),t(15)},1:function(n,r){},15:function(n,r){}});
